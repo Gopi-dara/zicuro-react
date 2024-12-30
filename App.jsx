69d1ec1c719f03cd6ab7e13aade55143ea2099f7
unknown <daragopi69@gmail.com>
2024-12-30 18:26:16 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+// App.js
+import React from "react";
+import MyEditor from "./DraftEditor.jsx";
+import DraftEditor from "./DraftEditor.jsx";
+
+function App() {
+  return (
+    <div style={{ margin: "20px" }}>
+      <h2>Demo Editor</h2>
+      <DraftEditor/>
+    </div>
+  );
+}
+
+export default App;
